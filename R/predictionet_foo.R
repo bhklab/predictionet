bf96460cc7bdb6bc5bea0481106f1711f16a8623
Benjamin Haibe-Kains <bhaibeka@jimmy.harvard.edu>
2011-05-27 15:29:43 -0400

    major changes in the vignette and minor changes in the documentation

--- R/predictionet_foo.R
+++ R/predictionet_foo.R
@@ -531,7 +531,7 @@ function(net, coefficients=FALSE) {
 ## coeffcients: if TRUE, the coefficients are extracted from the regression models for each existing edges,  boolean representing the presence of the edges otherwise
 ### returns adjacency matrix: parents in rows and children in columns (res[i,j]==1 means edge from i to j)
 `net2topo` <- 
-function(net, ...) {
+function(net, coefficients=FALSE, ...) {
 	switch(net$method, 
 	"regrnet"={
 		res <- regrnet2topo(net=net$net, ...)
