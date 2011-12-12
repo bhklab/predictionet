5c79ee20c08a1e4f2885bbdb1672412cadee37d1
Benjamin Haibe-Kains <bhaibeka@jimmy.harvard.edu>
2011-12-11 19:10:53 -0500

    add function net2pred

---
+++ R/net2pred.R
@@ -0,0 +1,24 @@
+## function to make the network model predictive either by fitting local regression models or by estimating CPTs
+`net2pred` <- 
+function(net, data, categories, predn, perturbations, method=c("linear", "linear.penalized", "cpt"), seed=54321) {
+	method <- match.arg(method)
+	if(missing(perturbations) || is.null(perturbations)) {
+		perturbations <- matrix(FALSE, nrow=nrow(data), ncol=ncol(data), dimnames=dimnames(data))
+	} else {
+		if(nrow(perturbations) == 1) {
+			perturbations[1, ] <- as.logical(perturbations[1, ])
+		} else { perturbations <- apply(perturbations, 2, as.logical) }
+		dimnames(perturbations) <- dimnames(data)
+	}
+	if(length(method) > 1) { stop("only one prediction can be specified!") }
+	switch(method, 
+	"linear"=, 
+	"linear.penalized"={
+		return(.build.regression.regrnet(net=net, data=data, predn=predn, perturbations=perturbations, regrmodel=method, seed=seed))
+	}, 
+	"cpt"={
+		stop("cpt method is not implemented yet!")
+	}, 
+	stop("no default parameter for method!")
+	)
+}
